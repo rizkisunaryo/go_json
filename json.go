dd0fc42f95d53cb63f51b7898bff18e0d2a4486c
Rizki Sunaryo <rizkisunaryo@gmail.com>
2016-04-10 22:25:39 +0700

    fix response

--- json.go
+++ json.go
@@ -12,9 +12,9 @@ func PrintJson(header string, w http.ResponseWriter, v interface{}) {
 	jsStr, err := json.Marshal(&v)
 	if err!=nil {
 		log.Println(header, "Message: Error in marshalling json: ", err.Error())
-		fmt.Fprintf(w, "{\"Message\":\"Error in marshalling json\"}")
+		fmt.Fprintln(w, "{\"Message\":\"Error in marshalling json\"}")
 	} else {
 		log.Println(newHeader, string(jsStr))
-		fmt.Fprintf(w, string(jsStr))
+		fmt.Fprintln(w, string(jsStr))
 	}
 }
